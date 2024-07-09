e467c4e8da78d44ec596d2f9c9cd1eba7df1d47c
알파 <arpa1705@gmail.com>
2024-07-09 22:02:25 +0900

    feat: first

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const audio = new Audio("/first.mp3");
+    audio.play();
+
+    const handleEnd = () => {
+      audio.src = "/second.mp3";
+      audio.loop = true;
+      audio.play();
+    };
+
+    audio.addEventListener("ended", handleEnd);
+
+    return () => {
+      audio.removeEventListener("ended", handleEnd);
+    };
+  });
+
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="w-full h-full"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
